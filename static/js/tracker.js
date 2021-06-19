f4e1ec9f170298a6e1c13f3dfb4c2c8710105fda
samughodake <samughodake1808@gmail.com>
2021-06-19 15:56:50 +0530

    changes made in vaccination page

--- static/js/tracker.js
+++ static/js/tracker.js
@@ -9,17 +9,11 @@ API().then(data => {
     console.log(data["Global"]); //global data
     var all_global_data = data.Global;
 
-    // console.log("Date " + all_global_data.Date);
-    // console.log("Total Confirmed: " + all_global_data.TotalConfirmed);
-    // console.log("Total Recovered: " + all_global_data.TotalRecovered);
-    // console.log("Total Deaths: " + all_global_data.TotalDeaths);
     document.querySelector(".total-confirmed").innerHTML += all_global_data.TotalConfirmed;
     document.querySelector(".total-recovered").innerHTML += all_global_data.TotalRecovered;
     document.querySelector(".total-deaths").innerHTML += all_global_data.TotalDeaths;
     document.querySelector(".new-confirmed").innerHTML += all_global_data.NewConfirmed;
     document.querySelector(".new-recovered").innerHTML += all_global_data.NewRecovered;
     document.querySelector(".new-deaths").innerHTML += all_global_data.NewDeaths;
-    // document.querySelector(".date1").innerHTML += all_global_data.Date;
-    // document.querySelector(".date2").innerHTML += all_global_data.Date;
 
 })
